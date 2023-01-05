19b2b958d1bf35d81d4128ae94e440d1de820324
nesrinecherif1310 <nessa131098@gmail.com>
2023-01-05 21:18:52 +0100

    add front

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const userRoute = require('./routers/user.router')
+
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin',"*")
+    res.setHeader('Access-Control-Request-Method',"*")
+    res.setHeader('Access-Control-Allow-Headers',"*")
+    next()
+})
 
 
 
@@ -15,4 +23,4 @@ app.use('/',userRoute)
 
 
 
-app.listen(3000,()=>console.log('server is ready'))
+app.listen(5000,()=>console.log('server is ready'))
